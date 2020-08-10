22d28c103c87dfcfa50ea6b5d471c2d35f45791e
rajitha302 <rajithadisanayaka999@gmail.com>
2020-08-10 09:39:59 +0530

    build

---
+++ build/precache-manifest.31604e503982c44e8f2679c28a4a0b54.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3847ad8663f045746eb4b4bd400e82d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "327a2f933b0651785b83",
+    "url": "/static/css/main.901787c1.chunk.css"
+  },
+  {
+    "revision": "0997fba19c46fe1b54a8",
+    "url": "/static/js/2.e6fde33d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.e6fde33d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "327a2f933b0651785b83",
+    "url": "/static/js/main.7b06ff39.chunk.js"
+  },
+  {
+    "revision": "461bc2b3b87f45cb2e60",
+    "url": "/static/js/runtime-main.40f09c02.js"
+  },
+  {
+    "revision": "70691a02419757707aaf4c8224fc721e",
+    "url": "/static/media/logo.70691a02.png"
+  }
+]);
